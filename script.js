3f0aa942bbc7548f2022969944bc0bc5fb966045
Tirath Bhathawala <tirath.bhathawala@gmail.com>
2023-08-20 18:40:15 +0530

    Add getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Tirath");
+
+function getComputerChoice() {
+    const randInt = Math.floor(Math.random() * 3) + 0;
+    const choices = ["Rock", "Paper", "Scissor"];
+    return choices[randInt];
+}
