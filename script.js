6d8fc91916c28635571a70c987fa0ec5f424de21
Tirath Bhathawala <tirath.bhathawala@gmail.com>
2023-08-20 18:51:04 +0530

    Add game() function

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissor"];
     return choices[randInt];
 }
+
+function game(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (
+        (playerSelection == "rock" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "scissor") ||
+        (playerSelection == "scissor" && computerSelection == "rock")
+    ) {
+        return "You Lose!";
+    }
+    else if (
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissor" && computerSelection == "paper") ||
+        (playerSelection == "rock" && computerSelection == "scissor")
+    ) {
+        return "You Win!";
+    }
+    else {
+        return "Draw!";
+    }
+}
+
+
+
+
